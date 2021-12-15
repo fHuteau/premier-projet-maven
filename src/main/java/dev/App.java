046e771b3c9750590ca17af4723931466a899cc2
Florian <florian.huteau@epsi.fr>
2021-12-15 19:26:17 +0100

    premier-projet-maven

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,20 @@
+package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
+    }
+}
